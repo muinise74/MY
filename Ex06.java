9e0c4947a8f7e8af23c152c52c0d0c1827313165
muinise74 <95010730+muinise74@users.noreply.github.com>
2021-11-27 15:19:41 +0900

    Add files via upload

---
+++ Ex06.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		// 손익분기점 - 노트북 -
+		// 운영비 : input1, 1대: 생산 비용 : input2 노트북 가격 : input3 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("운영비 : ");
+		long input1 = sc.nextInt();
+		System.out.print("1대 생산비 : ");
+		long input2 = sc.nextInt();
+		System.out.print("가격 : ");
+		long input3 = sc.nextInt();
+		
+		int cnt = 1;
+		while(true) {
+			if(input1+cnt*input2<cnt*input3) {
+				break;
+			}else
+				cnt++;
+		}
+		System.out.println("손익 분기점 : "+cnt);
+		sc.close();
+	}
+
+}
