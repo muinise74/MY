f7d37d14fd76f82fe76350ad801a8870b767225a
muinise74 <95010730+muinise74@users.noreply.github.com>
2021-12-13 12:21:10 +0900

    Update MemberDTO.java

--- src/MemberDTO.java
+++ src/MemberDTO.java
@@ -1,15 +1,43 @@
 
 public class MemberDTO {
-	// db¸¦ Ç¥ÇöÇÏ±âÀ§ÇÑ »ç¿ëÀÚ ÀÓÀÇÀÇ vo class
+	// dbë¥¼ í‘œí˜„í•˜ê¸°ìœ„í•œ ì‚¬ìš©ìž ìž„ì˜ì˜ vo class
 	// vo -> value object
-	String id;
-	String pw;
-	String nickName;
+	private String id;
+	private String pw;
+	private String nickName;
 	
 	public MemberDTO(String id, String pw, String nickName) {
 		super();
 		this.id = id;
 		this.pw = pw;
 		this.nickName = nickName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	
+	public void show() {
+		System.out.printf("ID : %s PW :%s NickName : %s\n",id,pw,nickName);
 	}
 }
