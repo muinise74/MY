689213e75575610aa515e926841d31cfb6ec9eb3
문도연 <95010730+muinise74@users.noreply.github.com>
2022-02-09 13:39:19 +0900

    Update LOL.java

--- 연습9_ㅋ/LOL.java
+++ 연습9_ㅋ/LOL.java
@@ -1,4 +1,4 @@
-package ¿¬½À9_¤»;
+package ì—°ìŠµ9_ã…‹;
 
 import java.awt.Color;
 import java.awt.GridLayout;
@@ -13,7 +13,7 @@ public class LOL extends JFrame implements Runnable{
 	static int[] ord = {0,1,2,3,4,5,6,7,8,17,26,35,44,53,62,71,80,79,78,77,76,75,74,73,72,63,54,45,36,27,18,9,10,11,12,13,14,15,16,25,34,43,52,61,70,69,68,67,66,65,64,55,46,37,28,19,20,21,22,23,24,33,42,51,60,59,58,57,56,47,38,29,30,31,32,41,50,49,48,39,40};
 	
 	public LOL() {
-		setTitle("Spiral");
+		setTitle("ã…‹");
 		setSize(360,365);
 		setResizable(true);
 		setLayout(new GridLayout(9,9,0,0));
@@ -43,7 +43,8 @@ public class LOL extends JFrame implements Runnable{
 				e.printStackTrace();
 			}
 			btns[ord[i]].setFont(new Font("San Serif",Font.BOLD,35));
-			btns[ord[i]].setText("¤»");
+			btns[ord[i]].setText("ã…‹");
+			btns[ord[(i+80)%81]].setText(" ");
 		}
 
 	}
